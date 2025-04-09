4c37a0476542eafbad1dcd926576e06f5d8da923
Ryosuke Hyakuta <38304912+ry0y4n@users.noreply.github.com>
2025-04-09 01:25:00 +0000

    updated our Django product to include our codespace name for the URL endpoint

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -7,11 +7,11 @@ from .models import User, Team, Activity, Leaderboard, Workout
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        'users': '/api/users/',
-        'teams': '/api/teams/',
-        'activities': '/api/activities/',
-        'leaderboard': '/api/leaderboard/',
-        'workouts': '/api/workouts/'
+        'users': 'https://crispy-spork-vg5xr4gwppp26g6j-8000.app.github.dev/api/users/',
+        'teams': 'https://crispy-spork-vg5xr4gwppp26g6j-8000.app.github.dev/api/teams/',
+        'activities': 'https://crispy-spork-vg5xr4gwppp26g6j-8000.app.github.dev/api/activities/',
+        'leaderboard': 'https://crispy-spork-vg5xr4gwppp26g6j-8000.app.github.dev/api/leaderboard/',
+        'workouts': 'https://crispy-spork-vg5xr4gwppp26g6j-8000.app.github.dev/api/workouts/'
     })
 
 class UserViewSet(viewsets.ModelViewSet):
